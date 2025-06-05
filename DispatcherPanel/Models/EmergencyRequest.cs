80e587b7804ac5aff62c69cb239600b4ffd808b9
ratten9fanger <zu.2017@yandex.ru>
2025-06-05 14:03:32 +0500

    Add project files.

---
+++ DispatcherPanel/Models/EmergencyRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DispatcherPanel.Models
+{
+    public class EmergencyRequest
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
